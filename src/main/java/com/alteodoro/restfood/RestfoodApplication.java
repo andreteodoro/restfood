d291514746faf331437c166674e630d378e2c38f
André Luiz Teodoro <andre.teodoro@eldorado.org.br>
2018-02-26 18:17:01 -0300

    first commit

---
+++ src/main/java/com/alteodoro/restfood/RestfoodApplication.java
@@ -0,0 +1,12 @@
+package com.alteodoro.restfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfoodApplication.class, args);
+	}
+}
