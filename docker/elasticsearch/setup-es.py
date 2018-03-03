f5f502a981517e42f8c661d0ba1d1d454a1c2794
André Luiz Teodoro <andre.teodoro@eldorado.org.br>
2018-03-03 00:49:36 -0300

    Setting up Elasticsearch and Docker

---
+++ docker/elasticsearch/setup-es.py
@@ -0,0 +1,66 @@
+from elasticsearch import Elasticsearch
+from retrying import retry
+import requests
+
+ES_HOST = {
+    "host" : "elasticsearch.local", 
+    "port" : 9200
+}
+RESTAURANTS_INDEX = 'restaurants'
+DISHES_INDEX = 'dishes'
+
+def retry_on_connectionerror(exc):
+    return isinstance(exc, requests.exceptions.ConnectionError)
+
+@retry(retry_on_exception=retry_on_connectionerror, wait_fixed=10000, stop_max_attempt_number=20)
+def check_if_elasticsearch_is_up(es):
+    res = requests.get('http://elasticsearch.local:9200')
+    if res.status_code == 200:
+        print('Elasticsearch is up and running!')
+
+def delete_indexes(es):
+    print("deleting '%s' index..." % (RESTAURANTS_INDEX))
+    res = es.indices.delete(index = RESTAURANTS_INDEX)
+    print(" response: '%s'" % (res))
+
+    print("deleting '%s' index..." % (DISHES_INDEX))
+    res = es.indices.delete(index = DISHES_INDEX)
+    print(" response: '%s'" % (res))
+
+def create_indexes(es):
+    print("creating '%s' index..." % (RESTAURANTS_INDEX))
+    res = es.indices.create(index = RESTAURANTS_INDEX)
+    print(" response: '%s'" % (res))
+
+    print("creating '%s' index..." % (DISHES_INDEX))
+    res = es.indices.create(index = DISHES_INDEX)
+    print(" response: '%s'" % (res))
+
+def bulk_indexes(es):
+    print("bulk indexing restaurants...")
+    restaurants_data = open('restaurants-data','r') 
+    res = es.bulk(index = RESTAURANTS_INDEX, body = restaurants_data.read())
+    print(" response: '%s'" % (res))
+
+    print("bulk indexing dishes...")
+    dishes_data = open('dishes-data','r') 
+    res = es.bulk(index = DISHES_INDEX, body = dishes_data.read())
+    print(" response: '%s'" % (res))
+
+def sanity_text(es):
+    print("sanity test...")
+    res = es.search(index = RESTAURANTS_INDEX, body = {"query": {"match_all": {}}})
+    print(" response: '%s'" % (res))
+
+def main():
+    es = Elasticsearch(hosts = [ES_HOST])
+
+    check_if_elasticsearch_is_up(es)
+    print(es.info)
+    if es.indices.exists(RESTAURANTS_INDEX) and es.indices.exists(DISHES_INDEX):
+        delete_indexes(es)
+    create_indexes(es)
+    bulk_indexes(es)
+    sanity_text(es)
+
+if __name__ == "__main__": main()
